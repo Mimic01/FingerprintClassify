55e09f0f7b87fe13a5d4fbfdbb1391c2f416d3f1
Alex <kyosqdro@gmail.com>
2020-02-19 22:28:56 -0300

    se agrega modelo base de cnn

--- FingerprintClassify.py
+++ FingerprintClassify.py
@@ -9,8 +9,18 @@ from numpy import load
 from numpy import asarray
 from keras.preprocessing.image import load_img
 from keras.preprocessing.image import img_to_array
+from keras.preprocessing.image import ImageDataGenerator
+from keras.utils import to_categorical
+from keras.models import Sequential
+from keras.layers import Conv2D
+from keras.layers import MaxPooling2D
+from keras.layers import Dense
+from keras.layers import Flatten
+from keras.optimizers import SGD
 from random import random, seed
 from shutil import copyfile
+import sys
+
 
 path = 'C:/FingerprintCNN/Datasets/NISTDB4/NISTSpecialDatabase4GrayScaleImagesofFIGS/sd04/png_txt/'
 
@@ -96,3 +106,38 @@ for file in pngfiles:
             adv += 1
             break
 
+def define_model():
+    model = Sequential()
+    model.add(Conv2D(32, (3,3), activation='relu', kernel_initializer='he_uniform', padding='same', input_shape=(512, 512, 3)))
+    model.add(MaxPooling2D(2, 2))
+    model.add(Flatten())
+    model.add(Dense(128, activation='relu', kernel_initializer='he_uniform'))
+    model.add(Dense(1, activation='sigmoid'))
+    opt = SGD(lr=0.001, momentum=0.9)
+    model.compile(optimizer=opt, loss='binary_crossentropy', metrics=['accuracy'])
+    return model
+def summarize_diagnostics(history):
+    pyplot.subplot(211)
+    pyplot.title('Cross Entropy Loss')
+    pyplot.plot(history.history['loss'], color='blue', label='train')
+    pyplot.plot(history.history['val_loss'], color='orange', label='test')
+    pyplot.subplot(212)
+    pyplot.title('Classification Accuracy')
+    pyplot.plot(history.history['accuracy'], color='blue', label='train')
+    pyplot.plot(history.history['val_accuracyu'], color='orange', label='test')
+    filename = sys.argv[0].split('/')[-1]
+    pyplot.savefig(filename + '_plot.png')
+    pyplot.close()
+
+    def run_test_harness():
+        model = define_model()
+        datagen = ImageDataGenerator(rescale=1.0/255)
+        train_it = datagen.flow_from_directory('urlToTrain', class_mode='categorical', batch_size=64, target_size=(512, 512))
+        test_it = datagen.flow_from_directory('urlToTest', class_mode='categorial', batch_size=64, target_size=(512, 512))
+        history = model.fit_generator(train_it, steps_per_epoch=len(train_it), validation_data=test_it, validation_steps=len(test_it), epochs=20, verbose=0)
+        _, acc = model.evaluate_generator(test_it, steps=len(test_it), verbose=0)
+        print('> %.3f' % (acc * 100.0))
+        summarize_diagnostics(history)
+        
+    run_test_harness()
+            
